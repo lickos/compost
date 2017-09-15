85fb10e750fb112ae3f070dacc85a6044705da57
lickos <george.chios@gmail.com>
2017-09-15 15:03:13 +0300

    Almost finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,15 @@ import { ErrorHandler, NgModule } from "@angular/core";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { StatusBar } from "@ionic-native/status-bar";
-import { AngularFireDatabaseModule } from "angularfire2/database";
-import { AngularFireModule } from "angularfire2";
 
 import { MyApp } from "./app.component";
 import { HomePage } from "../pages/home/home";
-import { FireproviderProvider } from "../providers/fireprovider/fireprovider";
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyC9EakqPdhKx3ZuyF_JaekJxW-h4s6fc4w",
-  authDomain: "compostsurvey-ba896.firebaseapp.com",
-  databaseURL: "https://compostsurvey-ba896.firebaseio.com",
-  projectId: "compostsurvey-ba896",
-  storageBucket: "compostsurvey-ba896.appspot.com",
-  messagingSenderId: "244956535059"
-};
 
 @NgModule({
   declarations: [MyApp, HomePage],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-    AngularFireDatabaseModule,
-    AngularFireModule.initializeApp(firebaseConfig)
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, HomePage],
-  providers: [StatusBar, SplashScreen, { provide: ErrorHandler, useClass: IonicErrorHandler }, FireproviderProvider]
+  providers: [StatusBar, SplashScreen, { provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,5 @@
-import { FireproviderProvider } from "./../../providers/fireprovider/fireprovider";
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
-import { AngularFireDatabase } from "angularfire2/database";
 
 @Component({
   selector: "page-home",
@@ -12,20 +10,9 @@ export class HomePage {
   showPrezi: boolean = false;
   showFirstButton: boolean = true;
   showSecondButton: boolean = false;
-  showerotimatologio: boolean = false;
-  showThirdButton: boolean = false;
-  showForthButton: boolean = false;
-  protiErotisi: string;
-  sex: string;
-  upValue;
-  prosopika: boolean = false;
-  showerotimatologio2: boolean = false;
+  erotimatologio: boolean = false;
 
-  constructor(
-    public navCtrl: NavController,
-    public afd: AngularFireDatabase,
-    public fp: FireproviderProvider
-  ) {}
+  constructor(public navCtrl: NavController) {}
 
   changeView() {
     this.showDim = false;
@@ -36,32 +23,7 @@ export class HomePage {
 
   changeView2() {
     this.showPrezi = false;
-    this.showerotimatologio = true;
+    this.erotimatologio = true;
     this.showSecondButton = false;
-    this.prosopika = true;
-  }
-
-  showButton() {
-    this.showThirdButton = true;
-  }
-
-  showButton2() {
-    this.showForthButton = true;
-  }
-
-  changeView3() {
-    if (this.protiErotisi == "andras") {
-      "einai 1";
-      this.sex = "ANDRES";
-    } else if (this.protiErotisi == "ginaika") {
-      this.sex = "GINAIKES";
-    }
-    this.fp.getitems(this.sex).then(data => {
-      this.upValue = data;
-      this.fp.setitems(this.sex, this.upValue);
-    });
-    this.showerotimatologio = false;
-    this.showThirdButton = false;
-    this.showerotimatologio2 = true;
   }
 }
